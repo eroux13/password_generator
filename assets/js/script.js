5a33546aeb6486b3ca3b43c5737c66dec0f0f6db
EJ <ej.roux13@gmail.com>
2021-01-14 15:48:10 -0800

    added use cases for criteria

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,9 @@ var numElement = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 // Special Characters
 var specialChar = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "=", "+"];
 
+// User choices
+var choices;
+
 // Write password to the #password input
 function writePassword() {
     var password = generatePassword();
@@ -27,21 +30,73 @@ function generatePassword() {
     if (!pwlength) {
         alert("Length cannot be blank!");
         generatePassword();
-    } else if (pwlength) {
-        while (pwlength < 8 || pwlength > 128) {
-            pwlength = prompt("Password length must be between 8 and 128 characters. Please enter a new length");
-            // Ask for criteria
-            var confirmUpper = confirm("Will there be Uppercase charaters?");
-            var confirmLower = confirm("Will there be Lowercase characters?");
-            var confirmNum = confirm("Will there be Numbers?");
-            var confirmSpecial = confirm("Will there be Special Characters?");
-            if (confirmUpper === false && confirmLower === false && confirmNum === false && confirmSpecial === false) {
-                alert("At least one criteria needs to be selected!");
-                console.log(confirmUpper);
-            }
+    } else if (pwlength < 8 || pwlength > 128) {
+        pwlength = prompt("Password length must be between 8 and 128 characters. Please enter a new length");
+        generatePassword();
+    } else if (pwlength >= 8 && pwlength <= 128) {
+        // Ask for criteria
+        var confirmUpper = confirm("Will there be Uppercase charaters?");
+        var confirmLower = confirm("Will there be Lowercase characters?");
+        var confirmNum = confirm("Will there be Numbers?");
+        var confirmSpecial = confirm("Will there be Special Characters?");
+        // Use Case for no inputs
+        if (confirmUpper === false && confirmLower === false && confirmNum === false && confirmSpecial === false) {
+            alert("At least one criteria needs to be selected!");
+            generatePassword();
         }
+        // Use case for all criteria
+        else if (confirmUpper && confirmLower && confirmNum && confirmSpecial) {
+            choices = capLetter.concat(lowLetter, numElement, specialChar);
+        }
+        // Use case for 3 criterias
+        else if (confirmUpper && confirmLower && confirmNum) {
+            choices = capLetter.concat(lowLetter, numElement);
+        }
+        else if (confirmUpper && confirmLower && confirmSpecial) {
+            choices = capLetter.concat(lowLetter, specialChar);
+        }
+        else if (confirmUpper && confirmSpecial && confirmNum) {
+            choices = capLetter.concat(specialChar, numElement);
+        }
+        else if (confirmLower && confirmSpecial && confirmNum) {
+            choices = lowLetter.concat(lowLetter, numElement);
+        }
+        // Use case for 2 criterias
+        else if (confirmUpper && confirmLower) {
+            choices = capLetter.concat(lowLetter);
+        }
+        else if (confirmUpper && confirmNum) {
+            choices = capLetter.concat(numElement);
+        }
+        else if (confirmUpper && confirmSpecial) {
+            choices = capLetter.concat(specialChar);
+        }
+        else if (confirmNum && confirmLower) {
+            choices = numElement.concat(lowLetter);
+        }
+        else if (confirmSpecial && confirmLower) {
+            choices = specialChar.concat(lowLetter);
+        }
+        else if (confirmNum && confirmSpecial) {
+            choices = numElement.concat(specialChar);
+        }
+        // Use case for 1 criteria
+        else if (confirmUpper) {
+            choices = capLetter;
+        }
+        else if (confirmLower) {
+            choices = lowLetter;
+        }
+        else if (confirmNum) {
+            choices = numElement;
+        }
+        else if (confirmSpecial) {
+            choices = specialChar;
+        }
+
     }
 }
+
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
